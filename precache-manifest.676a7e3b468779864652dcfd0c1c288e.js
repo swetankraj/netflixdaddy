857129b30f13243a866f0e183cae1e8bb08f0675
Swetank <iamswetank@gmail.com>
2019-11-17 07:30:54 +0530

    Updates

---
+++ precache-manifest.676a7e3b468779864652dcfd0c1c288e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab20bc344b6b7db4c93425f7b0fe866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "842e24220a68ccdd756d",
+    "url": "/static/css/main.d08f73c8.chunk.css"
+  },
+  {
+    "revision": "450679c739b4bd326d98",
+    "url": "/static/js/2.847920f6.chunk.js"
+  },
+  {
+    "revision": "842e24220a68ccdd756d",
+    "url": "/static/js/main.3409e720.chunk.js"
+  },
+  {
+    "revision": "608dbcc8663f278eccc0",
+    "url": "/static/js/runtime-main.3032167e.js"
+  },
+  {
+    "revision": "372568c41665d8c396f1da62f6b7a2a0",
+    "url": "/static/media/avengers.372568c4.jpg"
+  },
+  {
+    "revision": "09ef758df6dfb539772914aeaaafbfe6",
+    "url": "/static/media/deadpool-2.09ef758d.jpg"
+  },
+  {
+    "revision": "a1f673e7df715f16dae49f4874009082",
+    "url": "/static/media/netflix-logo.a1f673e7.png"
+  }
+]);
