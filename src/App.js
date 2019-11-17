ca6d3c1424d2bb2f656adb02a6f50251252dbe65
Swetank <iamswetank@gmail.com>
2019-11-17 09:46:38 +0530

    fix netflix logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import logo from "./img/netflix-logo.png";
+import logo from "./netflix-logo.png";
 import Nov2019 from "./content/Nov 2019/Nov2019";
 
 function App() {
